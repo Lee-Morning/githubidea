25ea29125b6d6f285a3aad4147f2678bc3d6d064
李晨 <1229878695@qq.com>
2020-09-26 23:14:08 +0800

    在IDEA中练习Git使用;演示分支

--- src/main/java/com/atguigu/bean/Love.java
+++ src/main/java/com/atguigu/bean/Love.java
@@ -3,5 +3,6 @@ package com.atguigu.bean;
 public class Love {
     public static void main(String[] args) {
         System.out.println("I love you!");
+        System.out.println("Me too!");
     }
 }
