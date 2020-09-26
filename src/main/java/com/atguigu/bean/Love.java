e8426e7ba99852bed80d7db7e1463515991a65a2
李晨 <1229878695@qq.com>
2020-09-26 23:17:29 +0800

    演示合并分支，产生冲突2

--- src/main/java/com/atguigu/bean/Love.java
+++ src/main/java/com/atguigu/bean/Love.java
@@ -3,6 +3,6 @@ package com.atguigu.bean;
 public class Love {
     public static void main(String[] args) {
         System.out.println("I love you!");
-        System.out.println("Me too!");
+        System.out.println("Me too!"+"Wonderful!");
     }
 }
